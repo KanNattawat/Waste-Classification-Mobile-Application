8508f15565325cd0df89fd98fa307ef865d3d090
Sahatsawat_Fame <108666123+Sundance02@users.noreply.github.com>
2025-09-18 17:42:33 +0700

    login/register

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 // import cookieParser from 'cookie-parser';
-import routes from './routes/routes.js';
+import auth_router from './routes/auth_routes.js';
+import router from './routes/routes.js'; 
+import { authMiddleware } from "./controller/auth_controller.js";
 
 dotenv.config();
 const app = express();
@@ -10,7 +12,8 @@ app.use(express.json());
 
 
 
-app.use("/", routes);
+app.use("/auth", auth_router);
+app.use("/", authMiddleware, router)
 
 app.listen(process.env.PORT, async () => {
     console.log('Service running');
