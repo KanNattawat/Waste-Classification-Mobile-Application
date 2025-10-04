c1d8f5e0ec916c629959dcab9aa98ccefd652cf8
KanNattawat <nattawat26907@gmail.com>
2025-10-04 21:57:21 +0700

    deploy setting

--- frontend/app/(tabs)/recents.tsx
+++ frontend/app/(tabs)/recents.tsx
@@ -45,7 +45,7 @@ const Recents = () => {
         return;
       }
 
-      const { data } = await axios.get("http://192.168.1.105:3000/gethistory", {
+      const { data } = await axios.get("http://3.27.46.182:3000/gethistory", {
         params: { userId },
       });
 
