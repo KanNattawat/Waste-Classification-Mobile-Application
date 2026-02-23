033e0755f44c84354b4ba2d2223ddbc052cdc59f
KanNattawat <nattawat26907@gmail.com>
2026-02-23 22:42:17 +0700

    add manage controller

--- backend/routes/manage_routes.js
+++ backend/routes/manage_routes.js
@@ -10,5 +10,10 @@ manage_router.post('/edituser',authMiddleware, adminCheck ,controller.editUser);
 manage_router.delete('/deleteuser',authMiddleware, adminCheck ,controller.deleteUser);
 manage_router.get('/waste',authMiddleware, adminCheck, controller.getWaste);
 
+manage_router.get("/getallitem", authMiddleware, adminCheck, controller.getPointShops);
+manage_router.post("/createitem", authMiddleware, adminCheck, controller.createPointShops);
+manage_router.put("/updateitem/:id", authMiddleware, adminCheck, controller.updatePointShop);
+manage_router.delete("/deleteitem/:id", authMiddleware, adminCheck, controller.deletePointShop);
+
 
 export default manage_router
